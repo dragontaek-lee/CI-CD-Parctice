89f950303c0738628dfdf79a8388e9c707ce4dca
unknown <dragontaek98@naver.com>
2022-12-28 18:09:34 +0900

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { NextFunction, Request, Response } from "express";
+
+const app = express(); 
+const PORT = 3000; 
+
+app.use(express.json()); 
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("server-running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
